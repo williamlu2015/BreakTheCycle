68522d34543f07a8e7111d2b20be7398c35c7e54
William <williamlu2015@outlook.com>
2018-01-13 16:28:16 -0800

    First commit with welcome page done. Adding vue.js next

---
+++ public/javascripts/main.js
@@ -0,0 +1,8 @@
+console.log("main.js is loaded")
+
+var c = document.getElementById("main--canvas");
+var ctx = c.getContext("2d");
+ctx.font = "30px Arial";
+ctx.fillText("Hello World",10,50);
+
+
---
+++ public/javascripts/welcome.js
@@ -0,0 +1,3 @@
+document.getElementById("start").addEventListener('click', () => {
+	document.getElementById("welcome--container").style.display = "none";
+});
